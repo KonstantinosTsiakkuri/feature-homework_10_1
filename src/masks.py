c03a13c28fbe7e77873a2e4ceb46d22009d3007f
Константинос Тсиаккури <olaspain@bk.ru>
2025-05-19 23:33:28 +0300

    feature/homework_10_2 Добавлены тесты и исправлены функции

--- src/masks.py
+++ src/masks.py
@@ -1,18 +1,16 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску."""
-    card_number_mask = "" + card_number[0:4] + " " + card_number[5:7] + "** **** " + card_number[12:]
+    cleaned_number = card_number.replace("A-", "").replace("-B", "").replace(" ", "").replace("-", "")
+    card_number_mask = cleaned_number[0:4] + " " + cleaned_number[4:6] + "** **** " + cleaned_number[-4:]
+    if card_number == "" or len(cleaned_number) != 16:
+        return "Некорректный ввод"
     return card_number_mask
-
-
-card_number = input()
-print(get_mask_card_number(card_number))
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску."""
-    account_mask = "**" + account_number[-4:]
+    cleaned_number = account_number.replace("A-", "").replace("-B", "")
+    account_mask = "**" + cleaned_number[-4:]
+    if account_number == "" or len(cleaned_number) < 16:
+        return "Некорректный ввод"
     return account_mask
-
-
-account_number = input()
-print(get_mask_account(account_number))
